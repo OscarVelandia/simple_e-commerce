c532614c0eb3fb58951894ff5d73bd3b9546ec8d
OscarVelandia <odvs@gmail.com>
2019-11-18 12:04:05 -0300

    Server * Quito la conexión de la db

--- server/source/server.js
+++ server/source/server.js
@@ -2,14 +2,13 @@ const express = require("express");
 const { json, urlencoded } = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
-const { connect } = require("./utils/db");
 const productRouter = require("./resources/product/product.router");
 
 const app = express();
 
 app.disable("x-powered-by");
 
-app.set("port", process.env.PORT || 3001);
+app.set("port", process.env.APP_PORT || 3001);
 
 app.use(cors());
 app.use(json());
@@ -20,7 +19,6 @@ app.use("/products", productRouter);
 
 const start = async () => {
   try {
-    await connect();
     app.listen(app.get("port"), () => {
       // eslint-disable-next-line no-console
       console.log(`REST API en http://localhost:${app.get("port")}`);
