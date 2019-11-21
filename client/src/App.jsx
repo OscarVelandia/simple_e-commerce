999e39236ea8e5fe34bc216fc26f544799d0744c
OscarVelandia <odvs@gmail.com>
2019-11-21 03:41:08 -0300

    Client * Agrego Página de error e importo fuente de google fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,16 @@ import { Router } from "@reach/router";
 import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 import ProductDetail from "./components/ProductDetail";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
     <>
       <Navbar />
       <Router>
+        <ErrorPage path="/error" />
         <ProductCard path="/" />
-        <ProductDetail path="/products/:id" />
+        <ProductDetail path="/products/:productId" />
       </Router>
     </>
   );
