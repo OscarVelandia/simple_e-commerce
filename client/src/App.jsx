2c8de00513460a158ce99548fab67c4b06d5b39c
OscarVelandia <odvs@gmail.com>
2019-11-20 23:50:32 -0300

    Client * Modifico la forma en que se agrega el router * Refactorizo el componente para que sirva como router solamente.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Router } from "@reach/router";
-import { Loader, Dimmer } from "semantic-ui-react";
+
 import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 import ProductDetail from "./components/ProductDetail";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const result = await axios.get("http://localhost:3001/products");
-        setProducts(result.data);
-      } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error("Error: ", err);
-      }
-    }
-
-    getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
-      <Navbar props={products} />
-      {products.length ? (
-        <ProductCard />
-      ) : (
-        <>
-          <Dimmer active>
-            <Loader indeterminate>Trayendo los productos</Loader>
-          </Dimmer>
-        </>
-      )}
-
+      <Navbar />
       <Router>
         <ProductCard path="/" />
         <ProductDetail path="/products/:id" />
